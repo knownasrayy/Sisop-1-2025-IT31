faffdff1efabb6131f5eddc5f4a603247ea23031
hanz006 <farhan11102006@gmail.com>
2025-03-21 18:52:42 +0700

    Update login.sh

--- Soal_2/login.sh
+++ Soal_2/login.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+if [ $# -ne 2 ]; then
+  echo "Penggunaan: $0 email password"
+  exit 1
+fi
+
+email="$1"
+password="$2"
+salt="ArcaeaSalt"
+
+hashed_password=$(echo -n "$password$salt" | sha256sum | cut -d' ' -f1)
+
+if grep -q "^$email,.*,$hashed_password$" data/player.csv; then
+  echo "Login berhasil!"
+else
+  echo "Login gagal. Email atau password salah."
+fi
